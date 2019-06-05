ef15e7b7d5e74c61b358e7673574cbc56650fd51
Kévin Meniel <kevin.meniel@gmail.com>
2019-06-05 23:14:21 +0900

    ImGui Events

--- Brioche/src/Brioche/ImGui/ImGuiLayer.h
+++ Brioche/src/Brioche/ImGui/ImGuiLayer.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include "Brioche/Layer.h"
+#include "Brioche/Events/Event.h"
+#include "Brioche/Events/ApplicationEvent.h"
+#include "Brioche/Events/KeyEvent.h"
+#include "Brioche/Events/MouseEvent.h"
 
 namespace Brioche {
 
@@ -14,6 +18,15 @@ namespace Brioche {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
